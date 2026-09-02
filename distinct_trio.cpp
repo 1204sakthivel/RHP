368574b66b0444a2dbb1b52a065264c05f91872b
1204sakthivel <1204sakthivel@gmail.com>
2026-09-02 19:06:01 +0530

    Add files via upload

---
+++ distinct_trio.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<long long> arr;
+
+    while(n--) {
+        long long i;
+        cin >> i;
+        arr.push_back(i);
+    }
+
+    sort(arr.begin(), arr.end());
+
+    long long ans = 0;
+
+    for(long long x : arr) {
+
+        auto itu = upper_bound(arr.begin(), arr.end(), x);
+        auto itl = lower_bound(arr.begin(), arr.end(), x);
+
+        long long idu = arr.end() - itu;   // elements > x
+        long long idl = itl - arr.begin(); // elements < x
+
+        ans += idu * idl;
+    }
+
+    cout << ans << endl;
+}
